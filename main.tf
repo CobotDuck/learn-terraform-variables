9e1b9c934080932ace945aea93b3931487c29ea7
TAPAN <cobot@duck.com>
2025-11-19 09:07:11 -0800

    fix providers

--- main.tf
+++ main.tf
@@ -10,11 +10,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
     }
   }
-  required_version = ">= 0.14.0"
-}
+}  
 
 provider "aws" {
   region  = "us-west-1"
