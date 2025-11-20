712dd06ef1c602b9522dc636862b78c68f9e7fc6
TAPAN <cobot@duck.com>
2025-11-20 07:01:19 -0800

    Test

--- main.tf
+++ main.tf
@@ -130,3 +130,8 @@ module "ec2_instances" {
 
 
 
+module "s3_bucket" {
+  source  = "app.terraform.io/policy-as-code-training/terraform-aws-s3-bucket-tapan/aws"
+  version = "1.0.0"
+  bucket_name = "terraform-aws-s3-bucket-tapan"
+}
